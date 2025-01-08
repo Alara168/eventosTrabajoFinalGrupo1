ba8b1c47c7a44f07184d8f81b8222b92a03d0916
Alara168 <alejandrolara168@gmail.com>
2025-01-08 11:40:41 +0100

    Finalizar eventos automaticamente

--- public/cargar_eventos.php
+++ public/cargar_eventos.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json'); // Establecer el tipo de contenido com
 
 try {
     // Consulta para obtener los eventos
-    $stmt = $pdo->prepare("SELECT titulo, fecha, horaInicio, horaFin, lugar, organizador, descripcion, registro FROM eventos ORDER BY fecha ASC");
+    $stmt = $pdo->prepare("SELECT titulo, fecha, horaInicio, horaFin, lugar, organizador, descripcion, registro FROM eventos WHERE finalizado = 0 ORDER BY fecha ASC");
     $stmt->execute();
 
     // Obtener los resultados en un arreglo asociativo
