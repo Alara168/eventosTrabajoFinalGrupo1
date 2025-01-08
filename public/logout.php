fc7af0fc9a334464b1e380148fba4a41d56a3e40
hcorr <hugouem19@gmail.com>
2025-01-08 13:01:16 +0100

    Logout clase

---
+++ public/logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start(); // Inicia la sesión
+
+// Destruye todas las variables de sesión
+$_SESSION = [];
+
+// Elimina la cookie de sesión si está configurada
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destruye la sesión
+session_destroy();
+
+// Redirige al usuario a la página de login o inicio
+header("Location: login.html");
+exit();
+?>
